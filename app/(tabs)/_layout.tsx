9784c65589bad681470c180df2dffa409a12c6c2
SamMiller <malatjitshepiso671@gmail.com>
2024-09-06 17:18:16 +0200

    update 7 files, delete 3 files and create 3 files

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,10 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Tabs } from "expo-router";
+import React from "react";
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { TabBarIcon } from "@/components/navigation/TabBarIcon";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { Image, View } from "react-native";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -11,25 +12,49 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        headerShown: true,
+        headerStyle: {
+          backgroundColor: Colors[colorScheme ?? "light"].background,
+        },
+        tabBarStyle: {
+          height: -20,
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+          title: "Home",
+          headerStyle: {
+            backgroundColor: Colors[colorScheme ?? "light"].background,
+            height:100,
+          },
+          headerLeft: () => (
+            <View className="ml-4 h-12 w-12 rounded-full bg-slate-300">
+              <Image
+                source={{
+                  uri: "https://randomuser.me/api/portraits/men/10.jpg",
+                }}
+                className="w-full h-full rounded-full"
+              />
+            </View>
           ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+          headerRight: () => (
+            <View className="mr-4 rounded-full p-2">
+              <TabBarIcon
+                name="settings-outline"
+                color={Colors[colorScheme ?? "light"].tint}
+              />
+            </View>
           ),
+          headerTitle: "MeetBudy",
+          headerTitleAlign: "center",
+          headerTitleStyle: {
+            fontWeight: "bold",
+            fontSize: 24,
+            color: Colors[colorScheme ?? "light"].tint.toString(),
+          },
         }}
       />
     </Tabs>
---
+++ app/(tabs)/story/[id].tsx
@@ -0,0 +1,11 @@
+import { View, Text, Image, TouchableOpacity } from "react-native";
+
+export default function Story() {
+  return (
+    <View className="flex-1 w-full h-full">
+      <View>
+        <Text>Hello</Text>
+      </View>
+    </View>
+  );
+}
